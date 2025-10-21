7ea24d609453fc494ac1481aebabd07289ff1d00
Andy <gabryelb027@gmail.com>
2025-10-21 16:21:35 -0500

    agregar formularios para crear y edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio - Florería</title>
-    {{--  @include('forms', ['Modo' => 'Encabezado'])  --}}
+    @include('forms', ['Modo' => 'Encabezado'])
 </head>
 <body>
     <div><h3>FLORERÍA ALESSA</h3></div>
