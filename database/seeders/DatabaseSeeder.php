bb2c61a8efcc489ea4795b8f59f5301cc3052d48
Andy <gabryelb027@gmail.com>
2025-11-07 15:20:57 -0500

    Pruebas 3.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,17 +17,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // Si no existe ningún usuario, crea el primero como administrador
-        if (User::count() === 0) {
-            User::create([
-                'name' => 'Administrador',
-                'surname' => 'Principal',
-                'phone' => '999999999',
-                'fecha_nacimiento' => '1990-01-01',
-                'email' => 'admin@floreria.com',
-                'password' => Hash::make('admin123'), // puedes cambiar la contraseña
-                'role' => 'admin',
-            ]);
-        }
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
