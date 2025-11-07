fba4f0dea263c60c4d8d62385fb9260421946e3a
Andy <gabryelb027@gmail.com>
2025-11-07 14:12:32 -0500

    crear usuario administrador predeterminado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,19 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+        // Si no existe ningún usuario, crea el primero como administrador
+        if (User::count() === 0) {
+            User::create([
+                'name' => 'Administrador',
+                'surname' => 'Principal',
+                'phone' => '999999999',
+                'fecha_nacimiento' => '1990-01-01',
+                'email' => 'admin@floreria.com',
+                'password' => Hash::make('admin123'), // puedes cambiar la contraseña
+                'role' => 'admin',
+            ]);
+        }
 
         User::factory()->create([
             'name' => 'Test User',
